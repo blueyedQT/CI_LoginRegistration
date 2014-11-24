d605eb947eb8c0db1612e810e5e3331be911775d
Katrina Sanford <blueyed_qt@hotmail.com>
2014-11-24 12:58:46 -0800

    Inital commit

---
+++ application/views/login_register.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login/Registration</title>
+</head>
+<body>
+	<form>
+		<h1>Log In</h1>
+		<p>Email: <input type="text" name="email" placeholder="Name"></p>
+		<p>Password: <input type="password" name="password"></p>
+		<input type="submit" name="login" value="Login">
+	</form>
+	<form>
+		<h1>Register</h1>
+		<p>First Name: <input type="text" name="first_name" placeholder="First Name"></p>
+		<p>Last Name: <input type="text" name="last_name" placeholder="Last Name"></p>
+		<p>Email Address: <input type="text" name="email" placeholder="xyz@yexample.com"></p>
+		<p>Password: <input type="password" name="password"></p>
+		<p>Confirm Password: <input type="password" name="password2"></p>
+		<input type="submit" name="register" value="Register">
+	</form>
+</body>
+</html>
